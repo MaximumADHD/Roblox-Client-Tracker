c9f93ed0e0e9b17699c2e5e458ae39c5a0fd6735
Roblox Client Tracker <rbxclientwatch@gmail.com>
2020-02-01 23:53:22 -0600

    0.417.0.376966 (Packages)

--- LuaPackages/Packages/_Index/roblox_purchase-prompt-a8988c3f-63e1d483/purchase-prompt/Flags/GetFFlagXboxGamepadIconFix.lua
+++
@@ -1,5 +0,0 @@
-game:DefineFastFlag("XboxGamepadIconFix", false)
-
-return function()
-	return game:GetFastFlag("XboxGamepadIconFix")
-end
--- LuaPackages/Packages/_Index/roblox_purchase-prompt-a8988c3f-63e1d483/purchase-prompt/Misc/Constants.lua
+++
@@ -1,9 +0,0 @@
-local Root = script.Parent.Parent
-
-local strict = require(Root.strict)
-
-return strict({
-	ABTests = strict({
-		ROBUX_HEX_ICON = "RobuxHexIconABTestName"
-	}, "Constants.ABTests")
-}, "Constants")
