a0ef3eca5fee9bbb6d0082272f3ad533fece8fce
Roblox Client Tracker <rbxclientwatch@gmail.com>
2021-01-20 12:06:41 -0600

    0.462.0.416719

--- shaders/include/Params.h
+++ shaders/include/Params.h
@@ -1,11 +1,6 @@
 struct Params
 {
-    vec4 TextureSize;
-    vec4 Params1;
-    vec4 Params2;
-    vec4 Params3;
-    vec4 Params4;
-    vec4 Params5;
-    vec4 Params6;
-    vec4 Bloom;
+    vec4 WaveParams;
+    vec4 WaterColor;
+    vec4 WaterParams;
 };
