49844d078ee32b59c6fc326ebdd3966fb582fcf4
Roblox Client Tracker <rbxclientwatch@gmail.com>
2024-03-01 10:44:40 -0600

    0.614.1.6140414 (Scripts)

--- scripts/CoreScripts/Modules/Flags/GetFFlagEnableSoundTelemetry.lua
+++
@@ -1,5 +0,0 @@
-game:DefineFastFlag("EnableSoundTelemetry3", false)
-
-return function()
-    return game:GetFastFlag("EnableSoundTelemetry3")
-end
--- scripts/CoreScripts/Modules/Flags/GetFIntSoundTelemetryThrottlingPercentage.lua
+++
@@ -1,5 +0,0 @@
-game:DefineFastInt("SoundTelemetryThrottlingPercentage", 0)
-
-return function()
-	return game:GetFastInt("SoundTelemetryThrottlingPercentage")
-end
