f811bb38b8fbf07d86db9cc6122f67b0c659094c
Roblox Client Tracker <rbxclientwatch@gmail.com>
2026-06-16 18:19:55 -0500

    0.726.0.7261140 (Compiled Scripts)

---
+++ CompiledPackages/Rodux/3.3.0/thunkMiddleware.luac.s
@@ -0,0 +1,11 @@
+MAIN:
+        0 PREPVARARGS                      0
+        1 GETIMPORT                        R0 K1 [require]
+        3 GETIMPORT                        R1 K3 [script]
+        5 GETTABLEKS                       R1 R1 K4 ["Parent"]
+        7 GETTABLEKS                       R1 R1 K5 ["makeThunkMiddleware"]
+        9 CALL                             R0 1 1
+       10 MOVE                             R1 R0
+       11 LOADNIL                          R2
+       12 CALL                             R1 1 -1
+       13 RETURN                           R1 -1
