2375fd87de014bc6250942eb4b5820ded3f4bede
Roblox Client Tracker <rbxclientwatch@gmail.com>
2026-06-30 15:18:20 -0500

    0.727.0.7271204 (Compiled Scripts)

--- CompiledPackages/DeveloperFramework/2dea4e6e/DeveloperFramework/UI/Components/RadioButtonList/style.luac.s
+++
@@ -1,6 +0,0 @@
-MAIN:
-        0 PREPVARARGS                      0
-        1 DUPTABLE                         R0 K1 [{"Padding"}]
-        2 LOADN                            R1 6
-        3 SETTABLEKS                       R1 R0 K0 ["Padding"]
-        5 RETURN                           R0 1
--- CompiledPackages/ReactReconciler/e1627717/ReactReconciler/ReactFiberReconciler.luac.s
+++
@@ -1,8 +0,0 @@
-MAIN:
-        0 PREPVARARGS                      0
-        1 GETIMPORT                        R0 K1 [require]
-        3 GETIMPORT                        R2 K4 [script]
-        5 GETTABLEKS                       R2 R2 K5 ["Parent"]
-        7 GETTABLEKS                       R1 R2 K2 ["ReactFiberReconciler.new"]
-        9 CALL                             R0 1 -1
-       10 RETURN                           R0 -1
